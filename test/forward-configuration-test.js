a8d44b1d00b79a975df6d093fe8c7a445bacdcaa
huzhou <huzhou@ebay.com>
2013-05-20 11:33:59 -0700

    + adding unit test for ForwardConfiguration

---
+++ test/forward-configuration-test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var ForwardConfiguration = require("../lib/forward-configuration.js").ForwardConfiguration,
+    ContextConfiguration = require("../lib/context-configuration.js").ContextConfiguration,
+    should = require("should"),
+    _ = require("underscore");
+
+describe("ForwardConfiguration", function() {
+
+    describe("#get(key)", function(){
+        var internal = new ContextConfiguration([], []);
+        internal.append({
+            "key":"k1",
+            "value":"v1"
+        });
+
+        var config = new ForwardConfiguration([internal]);
+
+        it("should get 'v1' using 'k1' ", function(done){
+            (config.get("k1")).should.equal("v1");
+            done();
+        });
+    });
+
+    describe("change", function(){
+        var internal = new ContextConfiguration([], []);
+        internal.append({
+            "key":"k1",
+            "value":"v1"
+        });
+
+        var config = new ForwardConfiguration([internal]);
+
+        it("should get notified on #swap", function(done){
+
+            var timeOut = setTimeout(function(){
+                done(new Error("time out"));
+            }, 1000);
+
+            config.on("change", function(forwards){
+                clearTimeout(timeOut);
+                forwards.should.not.be.empty;
+                done();
+            });
+
+            config.swap([internal]);
+        });
+    });
+});
