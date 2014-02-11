0fba9339adb405cdbad4d6b2fb32d76e10a471e9
Dmytro Semenov <dsemenov@ebay.com>
2014-02-11 14:58:04 -0800

    fixed bug that can happen in ConfigBuilder if two variations of the same config happen at the same time, race condition.

--- test/node-config-test.js
+++ test/node-config-test.js
@@ -3,6 +3,7 @@
 var EventEmitter = require("events").EventEmitter,
     NodeConfig = require("../lib/node-config.js").NodeConfig,
     Q = require("q"),
+    Builder = require("../lib/app.js").ConfigurationBuilder,
     should = require("should");
 
 
@@ -22,10 +23,10 @@ describe("NodeConfig", function(){
             emitter.on("config-read", function(message){
                 deferred.resolve(message);
             });
-
             deferred.promise.then(function(message){
                 should.exists(message, "message shouldn't be null");
                 (message.type).should.equal("config-read");
+                (message.target).should.equal("Global");
                 (message.config).should.equal("SRP");
                 (message.properties.length).should.equal(2);
                 done();
@@ -41,6 +42,62 @@ describe("NodeConfig", function(){
                 "config":"SRP",
                 "version":"1.0.0"
             });
+
+        });
+
+        it("should load config by ConfigBuilder", function(done){
+
+            var builder = new Builder(emitter);
+
+            // someone reads some other variation with different target
+            builder.build({}, {
+                "domain":"E|qa.ebay.com",
+                "target":"Target1",
+                "project":"search",
+                "config":"SRP",
+                "version":"1.0.0"
+            }).then(function(config) {
+                should.exists(config, "message shouldn't be null");
+                (config.get('k1')).should.equal("v1-t");
+            })
+            .fail(function(error){
+                done(error);
+            }).done();
+
+            // this is our read
+            builder.build({}, {
+                "domain":"E|qa.ebay.com",
+                "target":"Global",
+                "project":"search",
+                "config":"SRP",
+                "version":"1.0.0"
+            }).then(function(config) {
+                should.exists(config, "message shouldn't be null");
+                (config.get('k1')).should.equal("v1");
+                done();
+            })
+            .fail(function(error){
+                done(error);
+            }).done();
+
+            // simulate read of other config
+            new NodeConfig(process.cwd() + "/test/config", emitter).load({
+                "domain":"E|qa.ebay.com",
+                "target":"Target1",
+                "project":"search",
+                "config":"SRP",
+                "version":"1.0.0"
+            });
+
+            // simulate read of our config
+            new NodeConfig(process.cwd() + "/test/config", emitter).load({
+                "domain":"E|qa.ebay.com",
+                "target":"Global",
+                "project":"search",
+                "config":"SRP",
+                "version":"1.0.0"
+            });
+
         });
     });
 });
